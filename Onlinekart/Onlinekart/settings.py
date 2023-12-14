8f5de6139769fa1202f5a7c33053e05b11e7f078
TUSHAR-VERMA-star <83601065+TUSHAR-VERMA-star@users.noreply.github.com>
2023-12-14 22:49:03 +0530

    Update settings.py

--- Onlinekart/Onlinekart/settings.py
+++ Onlinekart/Onlinekart/settings.py
@@ -145,6 +145,6 @@ MESSAGE_TAGS = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tusharverma1510@gmail.com'
-EMAIL_HOST_PASSWORD = 'vsjmvkpazbzrwtzs'
+EMAIL_HOST_USER = '' #write your email id 
+EMAIL_HOST_PASSWORD = ''  #write you password
 EMAIL_USE_TLS = True
